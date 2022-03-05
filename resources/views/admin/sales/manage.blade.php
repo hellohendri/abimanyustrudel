26c86291ccd38a465138785c8f8410426e89060e
hellohendri <hendrinarendra@gmail.com>
2022-03-06 03:50:36 +0700

    coba feature sales

--- resources/views/admin/sales/manage.blade.php
+++ resources/views/admin/sales/manage.blade.php
@@ -1,11 +1,11 @@
 @extends("admin.layouts.default")
 @section('breadcrumbs')
-    <li class="breadcrumb-item active"><a href="{{ route("admin.sales.index") }}">Sales</a></li>
-    @if(isset($data))
-        <li class="breadcrumb-item active" aria-current="page">{{trans('admiko.page_breadcrumbs_edit')}}</li>
-    @else
-        <li class="breadcrumb-item active" aria-current="page">{{trans('admiko.page_breadcrumbs_add')}}</li>
-    @endIf
+<li class="breadcrumb-item active"><a href="{{ route("admin.sales.index") }}">Sales</a></li>
+@if(isset($data))
+<li class="breadcrumb-item active" aria-current="page">{{trans('admiko.page_breadcrumbs_edit')}}</li>
+@else
+<li class="breadcrumb-item active" aria-current="page">{{trans('admiko.page_breadcrumbs_add')}}</li>
+@endIf
 @endsection
 @section('pageTitle')
 <h1>Sales</h1>
@@ -22,17 +22,22 @@
         @if(isset($data)) @method('PUT') @endIf
         @csrf
         <div class="card-body">
-            @if ($errors->any())<div class="row"><div class="col-2"></div><div class="col"><div class="invalid-feedback d-block">@foreach($errors->all() as $error) {{$error}}<br> @endforeach</div></div></div>@endif
+            @if ($errors->any())<div class="row">
+                <div class="col-2"></div>
+                <div class="col">
+                    <div class="invalid-feedback d-block">@foreach($errors->all() as $error) {{$error}}<br> @endforeach</div>
+                </div>
+            </div>@endif
             <div class="row">
-                
+
                 <div class=" col-12">
                     <div class="form-group row">
                         <label for="nama" class="col-md-2 col-form-label">Nama:*</label>
                         <div class="col-md-10">
-                            <select class="form-select" id="nama" name="nama" required="true">
-                                
+                            <select class="form-select" id="nama" name="nama" required="true" data-placeholder="{{trans('admiko.select')}}" style="width: 100%" data-width="100%" data-allow-clear="true">
+
                                 @foreach($product_all as $id => $value)
-                                    <option value="{{ $id }}" {{ (old('nama') ? old('nama') : $data->nama ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
+                                <option value="{{ $id }}" {{ (old('nama') ? old('nama') : $data->nama ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
                                 @endforeach
                             </select>
                             <div class="invalid-feedback @if ($errors->has('nama')) d-block @endif">{{trans('admiko.required_text')}}</div>
@@ -46,9 +51,9 @@
                         <label for="metode_pembayaran" class="col-md-2 col-form-label">Metode Pembayaran:*</label>
                         <div class="col-md-10" style="position: relative">
                             <select class="form-select" id="metode_pembayaran" name="metode_pembayaran" required="true" data-placeholder="{{trans('admiko.select')}}" style="width: 100%" data-width="100%" data-allow-clear="true">
-                                
+
                                 @foreach($payment_method_all as $id => $value)
-                                    <option value="{{ $id }}" {{ (old('metode_pembayaran') ? old('metode_pembayaran') : $data->metode_pembayaran ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
+                                <option value="{{ $id }}" {{ (old('metode_pembayaran') ? old('metode_pembayaran') : $data->metode_pembayaran ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
                                 @endforeach
                             </select>
                             <div class="invalid-feedback @if ($errors->has('metode_pembayaran')) d-block @endif">{{trans('admiko.required_text')}}</div>
@@ -62,9 +67,9 @@
                         <label for="status_pembayaran" class="col-md-2 col-form-label">Status Pembayaran:*</label>
                         <div class="col-md-10">
                             <select class="form-select" id="status_pembayaran" name="status_pembayaran" required="true">
-                                
+
                                 @foreach($payment_status_all as $id => $value)
-                                    <option value="{{ $id }}" {{ (old('status_pembayaran') ? old('status_pembayaran') : $data->status_pembayaran ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
+                                <option value="{{ $id }}" {{ (old('status_pembayaran') ? old('status_pembayaran') : $data->status_pembayaran ?? '') == $id ? 'selected' : '' }}>{{ $value }}</option>
                                 @endforeach
                             </select>
                             <div class="invalid-feedback @if ($errors->has('status_pembayaran')) d-block @endif">{{trans('admiko.required_text')}}</div>
@@ -77,9 +82,7 @@
                     <div class="form-group row">
                         <label for="jumlah" class="col-md-2 col-form-label">Jumlah:*</label>
                         <div class="col-md-10">
-                            <input type="number" class="form-control limitPozNegNumbers numbersWidth" id="jumlah" name="jumlah" required="true" placeholder="Jumlah"
-                                   step="1" 
-                                   value="{{{ old('jumlah', isset($data)?$data->jumlah : '') }}}">
+                            <input type="number" class="form-control limitPozNegNumbers numbersWidth" id="jumlah" name="jumlah" required="true" placeholder="Jumlah" step="1" value="{{{ old('jumlah', isset($data)?$data->jumlah : '') }}}">
                             <div class="invalid-feedback @if ($errors->has('jumlah')) d-block @endif">{{trans('admiko.required_text')}}</div>
                             <small id="jumlah_help" class="text-muted"></small>
                         </div>
@@ -90,11 +93,7 @@
                         <label for="tanggal" class="col-md-2 col-form-label">Tanggal:*</label>
                         <div class="col-md-10">
                             <div class="input-group" id="dateTimePicker_tanggal" data-target-input="nearest">
-                                <input type="text" autocomplete="off" style="max-width: 170px;border-right: unset;"
-                                       data-date_time_format="{{config('admiko_config.form_date_time_format')}}"
-                                       class="form-control datetimepicker-input dateTimePicker"
-                                       data-target="#dateTimePicker_tanggal" required="true" id="tanggal" data-toggle="datetimepicker"
-                                       placeholder="Tanggal" name="tanggal" value="{{{ old('tanggal', isset($data)?$data->tanggal : '') }}}">
+                                <input type="text" autocomplete="off" style="max-width: 170px;border-right: unset;" data-date_time_format="{{config('admiko_config.form_date_time_format')}}" class="form-control datetimepicker-input dateTimePicker" data-target="#dateTimePicker_tanggal" required="true" id="tanggal" data-toggle="datetimepicker" placeholder="Tanggal" name="tanggal" value="{{{ old('tanggal', isset($data)?$data->tanggal : '') }}}">
                                 <div class="input-group-append input-group-text" data-target="#dateTimePicker_tanggal" data-toggle="datetimepicker">
                                     <i class="fas fa-calendar-alt fa-fw"></i>
                                 </div>

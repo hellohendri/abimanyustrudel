21e33361b04871ac8b840e33349ad93a02e178e7
hellohendri <hendrinarendra@gmail.com>
2022-03-06 00:29:34 +0700

    tambah card homepage

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -2,23 +2,36 @@
 @section('breadcrumbs')@endsection
 @section('pageTitle')
 <!-- <h1>{{ trans('admiko.home') }}</h1> -->
+<form action=" /" method="GET">
+    <div class="input-group mb-3" style="margin-bottom: 100px;">
+        <input type="date" class="form-control" name="start_date">
+        <input type="date" class="form-control" name="end_date">
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </div>
+</form>
 
 <div class="card production_history_index admikoIndex">
     <div class="card-body">
         <div class="row">
             <div class="col-sm-6 col-md-6 col-lg-3">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem;">
+                <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
                     <div class="card-body">
                         <h5 class="card-title">Total Omset</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h3 class="card-text">{{"Rp " . number_format($totalCost, 2, ".", ",")}}</h3>
+                    </div>
+                    <div class="card-footer">
+                        <small>Total Omset Penjualan</small>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4 col-md-6 col-lg-3">
                 <div class="card text-white bg-warning mb-3" style="max-width: 20rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Total Biaya Produksi</h5>
-                        <h5 class="card-text">{{"Rp " . number_format($totalCost, 2, ".", ",")}}</h5>
+                        <h5>Total Biaya Produksi</h5>
+                        <h3 class="card-text">{{"Rp " . number_format($totalCost, 2, ".", ",")}}</h3>
+                    </div>
+                    <div class="card-footer">
+                        <small>Total Biaya Produksi Pastry & Strudel</small>
                     </div>
                 </div>
             </div>
@@ -31,7 +44,7 @@
                 </div>
             </div>
             <div class="col-sm-6 col-md-6 col-lg-3">
-                <div class="card text-white bg-primary mb-3">
+                <div class="card text-white bg-info mb-3">
                     <div class=" card-body">
                         <h5 class="card-title">Total Laba / Rugi</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- resources/views/admin/production_history/index.blade.php
+++ resources/views/admin/production_history/index.blade.php
@@ -11,15 +11,15 @@
 <a href="{{route("admin.home")}}"><i class="fas fa-angle-left"></i> {{trans('admiko.page_back_btn')}}</a>
 @endsection
 @section('content')
+<form action="/" method="GET">
+    <div class="input-group mb-3">
+        <input type="date" class="form-control" name="start_date">
+        <input type="date" class="form-control" name="end_date">
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </div>
+</form>
 <div class="card production_history_index admikoIndex">
     <div class="card-body">
-        <form action="/" method="GET">
-            <div class="input-group mb-3">
-                <input type="date" class="form-control" name="start_date">
-                <input type="date" class="form-control" name="end_date">
-                <button class="btn btn-primary" type="submit">Submit</button>
-            </div>
-        </form>
         <div class="container" style="padding: 0px !important">
             <div class="row">
                 <div class="col-sm">
@@ -61,24 +61,25 @@
                     <div>
                         <div class="d-flex justify-content-start justify-content-sm-end">
                             <div class="searchTable">
-					<div class="input-group ps-2">
-                        <input type="text" name="admiko_search" class="form-control searchTableInput" placeholder="Search" value="">
-                    </div></div>
+                                <div class="input-group ps-2">
+                                    <input type="text" name="admiko_search" class="form-control searchTableInput" placeholder="Search" value="">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="tableLayout pb-2">
-                                <table class="table tableSort" style="width:100%" data-dom="ltrip">
+                <table class="table tableSort" style="width:100%" data-dom="ltrip">
                     <thead>
                         <tr data-sort-method='thead'>
-							<!-- <th scope="col" class="w-5" data-sort-method="number" >ID</th> -->
-							<th scope="col" class="text-nowrap">Nama</th>
-							<th scope="col" class="text-nowrap d-none d-sm-table-cell">Jenis</th>
-							<th scope="col" class="text-nowrap d-none d-md-table-cell">Jumlah</th>
-							<th scope="col" class="text-nowrap d-none d-lg-table-cell">HPP</th>
-							<th scope="col" class="d-none d-lg-table-cell">Tanggal Produksi</th>
-							<th scope="col" class="d-none d-lg-table-cell">Tanggal Expired</th>
+                            <!-- <th scope="col" class="w-5" data-sort-method="number" >ID</th> -->
+                            <th scope="col" class="text-nowrap">Nama</th>
+                            <th scope="col" class="text-nowrap d-none d-sm-table-cell">Jenis</th>
+                            <th scope="col" class="text-nowrap d-none d-md-table-cell">Jumlah</th>
+                            <th scope="col" class="text-nowrap d-none d-lg-table-cell">HPP</th>
+                            <th scope="col" class="d-none d-lg-table-cell">Tanggal Produksi</th>
+                            <th scope="col" class="d-none d-lg-table-cell">Tanggal Expired</th>
                             <th scope="col" class="w-5 no-sort" data-orderable="false">{{trans("admiko.table_edit")}}</th>
                             @if(Gate::allows('production_history_allow'))
                             <th scope="col" class="w-5 no-sort" data-orderable="false">{{trans('admiko.table_delete')}}</th>
@@ -86,30 +87,30 @@
                         </tr>
                     </thead>
                     <tbody>
-                    @foreach($tableData as $data)
+                        @foreach($tableData as $data)
                         <tr>
-							<!-- <td class="w-5"><a href="{{route("admin.production_history.edit",[$data->id])}}">{{$data->id}}</a></td> -->
-							<td class="text-nowrap">{{$data->nama}}</td>
-							<td class="text-nowrap d-none d-sm-table-cell">{{$data->jenis_id->jenis_roti??""}}</td>
-							<td class="text-nowrap d-none d-md-table-cell">{{$data->jumlah}}</td>
-							<td class="text-nowrap d-none d-lg-table-cell">{{$data->hpp}}</td>
-							<td class="d-none d-lg-table-cell">{{$data->tanggal_produksi}}</td>
-							<td class="d-none d-lg-table-cell">{{$data->tanggal_expired}}</td>
+                            <!-- <td class="w-5"><a href="{{route("admin.production_history.edit",[$data->id])}}">{{$data->id}}</a></td> -->
+                            <td class="text-nowrap">{{$data->nama}}</td>
+                            <td class="text-nowrap d-none d-sm-table-cell">{{$data->jenis_id->jenis_roti??""}}</td>
+                            <td class="text-nowrap d-none d-md-table-cell">{{$data->jumlah}}</td>
+                            <td class="text-nowrap d-none d-lg-table-cell">{{$data->hpp}}</td>
+                            <td class="d-none d-lg-table-cell">{{$data->tanggal_produksi}}</td>
+                            <td class="d-none d-lg-table-cell">{{$data->tanggal_expired}}</td>
                             <td class="w-5 no-sort"><a href="{{route("admin.production_history.edit",[$data->id])}}"><i class="fas fa-edit fa-fw"></i></a></td>
                             @if(Gate::allows(['production_history_allow']))
                             <td class="w-5 no-sort">
-                            <a href="#" data-id="{{$data->id}}" class="admiko_deleteConfirm" data-bs-toggle="modal" data-bs-target="#deleteConfirm"><i class="fas fa-trash fa-fw"></i></a>
-                        </td>
+                                <a href="#" data-id="{{$data->id}}" class="admiko_deleteConfirm" data-bs-toggle="modal" data-bs-target="#deleteConfirm"><i class="fas fa-trash fa-fw"></i></a>
+                            </td>
                             @endIf
                         </tr>
-                    @endforeach
+                        @endforeach
                     </tbody>
                 </table>
             </div>
             <div class="row">
                 <div class="col-12 col-sm order-3 order-sm-0 pt-2">
                     @if(Gate::any(['production_history_allow']))
-                        <a href="{{route('admin.production_history.create')}}" class="btn btn-primary" role="button"><i class="fas fa-plus fa-fw"></i> {{trans('admiko.table_add')}}</a>
+                    <a href="{{route('admin.production_history.create')}}" class="btn btn-primary" role="button"><i class="fas fa-plus fa-fw"></i> {{trans('admiko.table_add')}}</a>
                     @endIf
                 </div>
                 <div class="col-12 col-sm-auto order-0 order-sm-3 pt-2 align-self-center paginationInfo"></div>
@@ -122,24 +123,24 @@
     <div class="modal fade" id="deleteConfirm" tabindex="-1" role="dialog" aria-labelledby="deleteConfirm" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <form method="post" class="w-100" action="{{route("admin.production_history.delete")}}">
-            @method('DELETE')
-            @csrf
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{trans('admiko.delete_confirm')}}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                @method('DELETE')
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">{{trans('admiko.delete_confirm')}}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">{{trans('admiko.delete_message')}}</div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{trans('admiko.delete_close_btn')}}</button>
+                        <button type="submit" class="btn btn-danger deleteSoft">{{trans('admiko.delete_delete_btn')}}</button>
+                    </div>
                 </div>
-                <div class="modal-body">{{trans('admiko.delete_message')}}</div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{trans('admiko.delete_close_btn')}}</button>
-                    <button type="submit" class="btn btn-danger deleteSoft">{{trans('admiko.delete_delete_btn')}}</button>
-                </div>
-            </div>
-            <div class="dataDelete"></div>
+                <div class="dataDelete"></div>
             </form>
         </div>
     </div>
     @endIf
-    
+
 </div>
 @endsection
